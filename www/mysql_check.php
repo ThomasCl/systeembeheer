4a10d30cf9d73a9dd52aae4fb39fcf3796d591da
root <root@thomas-claessens.uclllabs.be>
2022-11-16 21:37:53 +0100

    zou moeten werken ma het werkt ni php

--- www/mysql_check.php
+++ www/mysql_check.php
@@ -2,25 +2,23 @@
 <body>
 
 <?php
-//$servername = "localhost";
-//$servername = "193.191.177.222";
-//$username = "check";
-//$password = "rDEetGxq82DCE";
-//$dbname = "check";
-//$conn = new mysqli($servername, $username, $password, $dbname);
-//if($conn -> connect_errno) {
-//	echo "Failed to connect to MySQL: " . $conn -> connect_error;
-//}
-//$sql = "select * from log order by id desc";
-//ECHO $sql
-//$query  = "SELECT text from log;";
-//$result = pg_query($conn, $sql);
-//if($result = $conn -> query("select * from check.log")){
-//	echo $result -> num_rows;
-//	$result -> free_result();
-//}
-//echo $sql
-//$conn -> close();
+
+$query = sprintf("SELECT * FROM check.log order by id desc limit 1");
+$result = mysql_query($query);
+
+if (!$result) {
+    $message  = 'Invalid query: ' . mysql_error() . "\n";
+    $message .= 'Whole query: ' . $query;
+    die($message);
+}
+
+while ($row = mysql_fetch_assoc($result)) {
+	echo $row['id'];
+    	echo $row['date'];
+    	echo $row['text'];
+}
+mysql_free_result($result);
+
 ?>
 </body>
 </html>
