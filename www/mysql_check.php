05723cbed0412dae721bbafbb68ac460b7af6685
root <root@thomas-claessens.uclllabs.be>
2022-11-16 21:30:42 +0100

    php is ambetant

--- www/mysql_check.php
+++ www/mysql_check.php
@@ -3,24 +3,24 @@
 
 <?php
 //$servername = "localhost";
-$servername = "193.191.177.222";
-$username = "check";
-$password = "rDEetGxq82DCE";
-$dbname = "check";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if($conn -> connect_errno) {
-	echo "Failed to connect to MySQL: " . $conn -> connect_error;
-}
+//$servername = "193.191.177.222";
+//$username = "check";
+//$password = "rDEetGxq82DCE";
+//$dbname = "check";
+//$conn = new mysqli($servername, $username, $password, $dbname);
+//if($conn -> connect_errno) {
+//	echo "Failed to connect to MySQL: " . $conn -> connect_error;
+//}
 //$sql = "select * from log order by id desc";
 //ECHO $sql
 //$query  = "SELECT text from log;";
 //$result = pg_query($conn, $sql);
-if($result = $conn -> query("select * from check.log")){
-	echo $result -> num_rows;
-	$result -> free_result();
-}
+//if($result = $conn -> query("select * from check.log")){
+//	echo $result -> num_rows;
+//	$result -> free_result();
+//}
 //echo $sql
-$conn -> close();
+//$conn -> close();
 ?>
 </body>
 </html>
